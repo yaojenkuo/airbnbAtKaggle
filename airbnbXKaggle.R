765e11cd3bb34bffa956b839b8dd9f1c46917f48
Yao-Jen <tonykuoyj@gmail.com>
2015-12-15 16:29:03 +0800

    added contents

--- airbnbXKaggle.R
+++ airbnbXKaggle.R
@@ -11,6 +11,7 @@ testUsers <- read.table('test_users.csv', sep=',', stringsAsFactors=T, header=T)
 # libraries
 library(ggplot2)
 library(VIM)
+library(sqldf)
 
 # merge ageSummary and countriesSummary
 ageCountries <- merge(ageSummary, countriesSummary, by="country_destination", all.x=TRUE)
@@ -25,3 +26,41 @@ sessionsTrain <- merge(sessions, trainUsers, by.x="user_id", by.y="id")# inner j
 sessionsTest <- merge(sessions, testUsers, by.x="user_id", by.y="id")# inner join
 write.csv(sessionsTrain, "c:/sessionsTrain.csv", row.names=FALSE)
 write.csv(sessionsTest, "c:/sessionsTest.csv", row.names=FALSE)
+
+# Build ABT for classification algorithm
+# Using sqldf to manipulate dataset
+sessions$action_type[sessions$action_type==''] <- '-unknown-'
+sessionsActionType <- sqldf("select user_id, 
+                 case when action_type=='booking_request' then 'booking_request'
+                 when action_type=='booking_response' then 'booking_response'
+                 when action_type=='click' then 'click'
+                 when action_type=='data' then 'data'
+                 when action_type=='message_post' then 'message_post'
+                 when action_type=='modify' then 'modify'
+                 when action_type=='partner_callback' then 'partner_callback'
+                 when action_type=='submit' then 'submit'
+                 when action_type=='view' then 'view'
+                 else 'other_action_types' end as action_type,
+                 sum(secs_elapsed) as secs_elapsed
+                 from sessions
+                 group by user_id, action_type
+                 ")
+sessionsDeviceType <- sqldf("select user_id,
+                            case when device_type=='Android App Unknown Phone/Tablet' then 'android_app_unknown_phone_tablet'
+                            when device_type=='Android Phone' then 'android_phone'
+                            when device_type=='Blackberry' then 'blackberry'
+                            when device_type=='Chromebook' then 'chromebook'
+                            when device_type=='iPad Tablet' then 'iPad_tablet'
+                            when device_type=='iPodtouch' then 'iPodtouch'
+                            when device_type=='Linux Desktop' then 'linux_desktop'
+                            when device_type=='Mac Desktop' then 'mac_desktop'
+                            when device_type=='Opera Phone' then 'opera_phone'
+                            when device_type=='Tablet' then 'tablet'
+                            when device_type=='Windows Desktop' then 'windows_desktop'
+                            when device_type=='Windows Phone' then 'windows_phone'
+                            else 'other_device_types' end as device_type,
+                            sum(secs_elapsed) as secs_elapsed
+                            from sessions
+                            group by user_id, device_type
+                            ")
+# use reshape2 package to 'dcast' the above 2 tables separately
